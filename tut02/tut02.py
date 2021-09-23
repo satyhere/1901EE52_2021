fc19dacb179eb0fcaa66d3cd209a0275f4eceb5e
Satyam K <satyam_1901ee52@iitp.ac.in>
2021-09-23 19:57:52 +0530

    Updated tut02.py

--- tut02/tut02.py
+++ tut02/tut02.py
@@ -1,4 +1,30 @@
 def get_memory_score():
-    return
+    rdef memory_score(input_nums) :
+    memory=[]
+    ans=0
+        for i in input_nums :
+            if len(memory) ==0:
+            memory.append(i)
+            else :
+                present =0
+            for j in memory :
+                if j==i :
+                    present=1
+            if present :
+                ans+=1
+            else :
+                memory.append(i)
+                if len(memory)==6 :
+                    memory.pop(0)
+    return ans
 
-input_nums = [3, 4, 5, 3, 2, 1]
+    input_nums=[3,4,3,0,7,4,5,2,1,3]
+    f=1
+    for i in input_nums :
+        if type(i)!=int :
+            f=0
+
+    if f==0 :
+        print("Please enter a valid input list")
+    else :
+        print(memory_score(input_nums))
